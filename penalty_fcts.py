9bfc56a360677b95bab1106675ea07534c518d1a
wolgast <thomaswolgast@web.de>
2019-11-30 17:51:10 +0100

    Minor changes in penalty functions

--- penalty_fcts.py
+++ penalty_fcts.py
@@ -5,10 +5,11 @@ A collection of penalty functions to punish selected constraint violations.
 """
 
 
-def penalty_fct(net, constraints):
+def penalty_fct(net, constraints: list):
     """ Punish a set of constraints. Possible are: voltage band violation
     (String: 'voltage_band'), max line loading ('line_load'), max trafo
-    loading ('trafo_load' and/or 'trafo3w_load'). """
+    loading ('trafo_load' and/or 'trafo3w_load'), max apparent power of 
+    generators('apparent_power'). """
     # TODO: Add option to make penelty adjustable! -> ((constraint1, penalty1) ...) ?
 
     if isinstance(constraints, str):
@@ -16,13 +17,11 @@ def penalty_fct(net, constraints):
             return 0, True
         elif constraints == 'all':
             # Attention: does not include 'apparent_power', because it is not
-            # included in original pandapower
+            # included in original pandapower constraints 
             constraints = ('voltage_band', 'line_load',
                            'trafo_load', 'trafo3w_load')
 
-    penalty = 0
-    for constraint in constraints:
-        penalty += eval(constraint)(net)
+    penalty = sum([eval(constraint)(net) for constraint in constraints])
 
     # Define under which circumstances a solution is seen as valid
     if penalty > 0:
@@ -33,14 +32,12 @@ def penalty_fct(net, constraints):
     return penalty, valid
 
 
-def voltage_band(net):
+def voltage_band(net, costs = 1000000):
     """ Punish voltage violations with 1 Meuro per 1pu violation.
     See https://pandapower.readthedocs.io/en/v2.1.0/opf/formulation.html for
     default voltage band values. """
     # TODO: divide upper and lower boundary into two functions?
-    # TODO: Make costs adjustable!
-    costs = 1000000
-
+    
     penalty = 0
     for idx in net.bus.index:
 
@@ -55,22 +52,22 @@ def voltage_band(net):
     return penalty
 
 
-def line_load(net):
+def line_load(net, costs=10000):
     """ Punish line load violation with 10 keuro per 1% violation. """
-    return loading(net, costs=10000, unit_type='line')
+    return loading(net, costs=costs, unit_type='line')
 
 
-def trafo_load(net):
+def trafo_load(net, costs=10000):
     """ Punish trafo load violation with 10 keuro per 1% violation. """
-    return loading(net, costs=10000, unit_type='trafo')
+    return loading(net, costs=costs, unit_type='trafo')
 
 
-def trafo3w_load(net):
+def trafo3w_load(net, costs=10000):
     """ Punish trafo load violation with 10 keuro per 1% violation. """
-    return loading(net, costs=10000, unit_type='trafo3w')
+    return loading(net, costs=costs, unit_type='trafo3w')
 
 
-def loading(net, costs, unit_type: str='trafo'):
+def loading(net, costs, unit_type: str):
     """ Punish load violation of trafo or line with 'costs' per 1%
     violation. """
     penalty = 0
@@ -93,6 +90,5 @@ def apparent_power(net, costs=10000):
         for s in s_sgen:
             if s > net[gen_type].max_s_mva[idx]:
                 penalty += (s - net[gen_type].max_s_mva[idx]) * costs
-
 
     return penalty
